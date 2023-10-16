097299bde98fdad4d786964eac236299820e0eac
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-10-16 15:22:17 +0200

    Initial commit

---
+++ generateur.py
@@ -0,0 +1,19 @@
+"""
+Voici une proposition de structure sémantique générique de TED talk :
+
+"
+%STRUCTURE
+"
+
+A partir de cette structure, rédige discours complet sur le thème de %TOPIC. 
+Voici des infos sur %TOPIC. 
+Le format du talk doit être rédigé avec des phrases completes. Sans titre, sans bullet point. 
+Il doit durer 15 minutes (soit 1500 à 2000 mots). 
+Le discours doit pouvoir etre lu tel quel.
+Voici des informations sur %TOPIC à utiliser pour construire le discours :
+
+"
+%DATA_TOPIC
+"
+    
+"""
